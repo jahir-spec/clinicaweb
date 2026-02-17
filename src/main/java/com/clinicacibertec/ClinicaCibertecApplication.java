cc08bb67cc77ceba8e77b7be06b980e8471ad0a6
IronInkai <IronInkai@github.com>
2026-02-17 11:28:39 -0500

    Inicio del proyecto

---
+++ src/main/java/com/clinicacibertec/ClinicaCibertecApplication.java
@@ -0,0 +1,13 @@
+package com.clinicacibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaCibertecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaCibertecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicacibertec/ClinicaCibertecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicacibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaCibertecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
